ed362c29172974240255894c14750f0d9cf7bd9d
Aakash Deep Gupta <aakashdeepgupta18@gmail.com>
2021-08-05 21:23:22 +0530

    added solution to bestSum problem in dynamic programming

---
+++ Dynamic-Programming/Memoization/bestsum.py
@@ -0,0 +1,19 @@
+'''
+------MEMOIZATION -----
+
+    MEMOIZATION IS A TECHNIQUE USED TO MAKE PROGRAMMING SOLUTIONS 
+    MORE EFFECTIVE , FAST AND EFFICIENT BY STORING RESULT OF
+    EACH CALL IN A DATA STRUCTURE  CALLED MEMOIZING
+
+Q write a function bestsum(targetsum,numbers) that takes
+in a target sum and an array of numbers as arguments.
+the function should return an array containing the shortest 
+combination of numbers that add up to exactly the target 
+sum 
+if there is a tie for the shortest combination , you may 
+return any one of the shortest
+
+'''
+
+def bestsum(targetsum,numbers):
+    pass
